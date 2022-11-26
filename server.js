d61a18d0d7e083f1b3e674af0e5f85d073940156
vedatozayar <vedatozayar@gmail.com>
2022-11-26 17:18:10 +0200

    auth-server setup

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ import jobsRouter from './routes/jobsRouter.js';
 //middleware
 import notFoundMiddleware from './middleware/not-found.js';
 import errorHandlerMiddleware from './middleware/error-handler.js';
+import authenticateUser from './middleware/auth.js';
+
 if (process.env.NODE_ENV !== 'production') {
   app.use(morgan('dev'));
 }
@@ -31,7 +33,7 @@ app.get('/api/v1', (req, res) => {
 });
 
 app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/jobs', jobsRouter);
+app.use('/api/v1/jobs', authenticateUser, jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
